7b742a99b181edfba46a82ac97a0ae602f88b563
Unknown <andrewrominger@outlook.com>
2018-12-06 17:58:54 -0800

    Readability cleanup

--- Kolibri.Lib/ModInjecter.cs
+++ Kolibri.Lib/ModInjecter.cs
@@ -14,15 +14,19 @@ namespace Kolibri.Lib
         private readonly DefaultAssemblyResolver _resolver;
         private readonly DirectoryInfo _gameDirectory;
         private readonly DirectoryInfo _gameAssemblyDirectory;
-        private readonly DirectoryInfo[] _modDirectories;
         private readonly FileInfo _gameAssemblyFile;
         private readonly FileInfo _backupGameAssemblyFile;
         private readonly List<FileInfo> _modAssemblies = new List<FileInfo>();
+
         public ModInjecter(string gameDirectory)
         {
             _gameDirectory = new DirectoryInfo(gameDirectory);
-            _modDirectories = _gameDirectory.GetDirectories("Mods").Single().GetDirectories();
-            foreach (var modDirectory in _modDirectories)
+            var modDirectories = _gameDirectory
+                .GetDirectories("Mods")
+                .Single()
+                .GetDirectories();
+
+            foreach (var modDirectory in modDirectories)
             {
                 _modAssemblies.AddRange(modDirectory.GetFiles($"{modDirectory.Name}.dll"));
             }
@@ -32,21 +36,32 @@ namespace Kolibri.Lib
                 .FirstOrDefault()?
                 .GetDirectories("Managed")
                 .FirstOrDefault();
+
+            if(_gameAssemblyDirectory == null)
+                throw new NullReferenceException("Game's data directory was not found");
+
             _resolver = new DefaultAssemblyResolver();
             _resolver.AddSearchDirectory(_gameAssemblyDirectory.FullName);
             _resolver.AddSearchDirectory(_gameDirectory.FullName);
-            _gameAssemblyFile = _gameAssemblyDirectory.GetFiles("Assembly-CSharp.dll").Single();
+
+            _gameAssemblyFile = _gameAssemblyDirectory
+                .GetFiles("Assembly-CSharp.dll")
+                .Single();
+
             _backupGameAssemblyFile = new FileInfo(Path.Combine(_gameAssemblyDirectory.FullName, $"Assembly-CSharp-Backup.dll"));
+
             if (!_backupGameAssemblyFile.Exists)
             {
                 var gameAssembly = AssemblyDefinition.ReadAssembly(_gameAssemblyFile.FullName,
                     new ReaderParameters() {AssemblyResolver = _resolver});
                 gameAssembly.Write(_backupGameAssemblyFile.FullName);
             }
+
             if (_backupGameAssemblyFile.Exists)
             {
                 File.Copy(_backupGameAssemblyFile.FullName, _gameAssemblyFile.FullName, true);
             }
+
             LoadUnityAssemblies();
             foreach (var modAssembly in _modAssemblies)
             {
@@ -72,10 +87,23 @@ namespace Kolibri.Lib
                    
                 foreach (var methodInjection in mod.ModMethodInjections)
                 {
-                    var toInject = modAssembly.MainModule.GetType(methodInjection.SourceMethod.InjectionType).Methods.Single(m => m.Name == methodInjection.SourceMethod.InjectionMethod);
-                    var injectionLocation = gameAssembly.MainModule.GetType(methodInjection.TargetMethod.InjectionType).GetMethod(methodInjection.TargetMethod.InjectionMethod);
+                    var toInject = modAssembly
+                        .MainModule
+                        .GetType(methodInjection.SourceMethod.InjectionType)
+                        .Methods
+                        .Single(m => m.Name == methodInjection.SourceMethod.InjectionMethod);
+
+                    var injectionLocation = gameAssembly
+                        .MainModule
+                        .GetType(methodInjection.TargetMethod.InjectionType)
+                        .GetMethod(methodInjection.TargetMethod.InjectionMethod);
+
                     var injector = new InjectionDefinition(injectionLocation, toInject, methodInjection.InjectFlags);
-                    injector.Inject(methodInjection.InjectionLocation == MethodInjectionInfo.MethodInjectionLocation.Top ? injectionLocation.Body.Instructions.First() : injectionLocation.Body.Instructions.Last());
+
+                    injector
+                        .Inject(methodInjection.InjectionLocation == MethodInjectionInfo.MethodInjectionLocation.Top
+                               ? injectionLocation.Body.Instructions.First()
+                               : injectionLocation.Body.Instructions.Last());
                 }
                 File.Copy(mod.ModAssemblyFile.FullName, Path.Combine(_gameDirectory.FullName, mod.ModAssemblyFile.Name), true);
                 File.Copy(mod.ModAssemblyFile.FullName, Path.Combine(_gameAssemblyDirectory.FullName, mod.ModAssemblyFile.Name), true);
